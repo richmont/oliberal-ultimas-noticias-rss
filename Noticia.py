0b5ad42fb1e83f751aa482410274c628e0d7bd3e
Richelmy Monteiro <richelmy.monteiro@gmail.com>
2022-09-19 21:09:27 -0300

    teste unitário para classe Noticia feito em pytest

--- Noticia.py
+++ Noticia.py
@@ -1,14 +1,82 @@
+
+from beartype import beartype
+import re
+import logging
+
 class Noticia():
-    def __init__(self, conteudo) -> None:
+    @beartype
+    def __init__(self, conteudo: dict) -> None:
         
-        self.titulo = conteudo["titulo"]
-        self.url = conteudo["url"]
+        self.titulo = self.recebe_titulo(conteudo["titulo"])
+        self.url = self.recebe_url(conteudo["url"])
         self.chamada = conteudo["chamada"]
         self.data = conteudo["data"]
-        self.imagem_url = conteudo["url_imagem"]
+        self.imagem_url = self.recebe_imagem_url(conteudo["url_imagem"])
 
+    @beartype
+    def recebe_titulo(self, titulo: str) -> str:
+        """Verifica se o primeiro caractere do título é em maiúscula
+
+        Args:
+            titulo (str): Título recebido
+
+        Raises:
+            errors.Titulo_nao_capitalizado: 
+
+        Returns:
+            str: Título retornado
+        """
+        if titulo[0].isupper():
+            return titulo
+        else:
+            raise errors.Titulo_nao_capitalizado("Título não começa com letra maiúscula: %s", titulo)
+
+    def recebe_url(self, url: str) -> str:
+        """Verifica se a URL possui 'http' no início, sobe exceção se negativo
+
+        Args:
+            url (str): url referente a notícia
+
+        Raises:
+            errors.Url_sem_http: Exceção quando url não possui http no começo
+
+        Returns:
+            str: url caso passe no teste
+        """
+        if url[0:4] == "http":
+            return url
+        else:
+            raise errors.Url_sem_http("Url recebido não possui 'http' no começo, inválido: %s", url)
+
+    def recebe_imagem_url(self, imagem_url: str) -> str:
+        """Verifica se URL da imagem possui http no começo e formato de extensão de imagem
+
+        Args:
+            imagem_url (str): endereço que aponta para imagem de notícia
+
+        Raises:
+            errors.Imagem_url_sem_extensao: 
+
+        Returns:
+            str: imagem_url
+        """
+        imagem_url = self.recebe_url(imagem_url)
+        ocorrencias_formato = re.findall(r'.jpg|.png|.jpeg|.gif|.jfif', imagem_url, re.IGNORECASE)
+        if any(ocorrencias_formato):
+            return imagem_url
+        else:
+            raise errors.Imagem_url_sem_extensao("URL da imagem não possui extensão suportada: %s", imagem_url)
+
+
+class errors():
+    class Titulo_nao_capitalizado(Exception):
+        pass
+    class Url_sem_http(Exception):
+        pass
+    class Imagem_url_sem_extensao(Exception):
+        pass
 
 if __name__ == "__main__":
-    dicionario = {"titulo": "iuashdiausdh", "url": "aisudhiads", "chamada": "aiushdasid","data": "aisuhdiuads", "url_imagem": "aisuhdiaus"}
+    dicionario = {"titulo": "Auashdiausdh", "url": "http://aisudhiads", "chamada": "aiushdasid", "data": "aisuhdiuads", "url_imagem": "http://ais.JPGuhdiaus"}
     noticia = Noticia(dicionario)
-    print(noticia.titulo)
+    print(noticia.imagem_url)
---
+++ test/test_Noticia.py
@@ -0,0 +1,65 @@
+from Noticia import Noticia, errors
+import pytest
+
+class Test_noticia_correta():
+    def dict_noticia_ok(self):
+        dicionario_ok = {
+            "titulo": "Lorem ipsum",
+            "url": "http://aisudhiads",
+            "chamada": "asdasdad",
+            "data": "asdadads",
+            "url_imagem": "http://asduihasd.jpg"
+        }
+        return dicionario_ok
+
+    def test_noticia_ok(self):
+        noticia = Noticia(self.dict_noticia_ok())
+
+class Test_noticia_incorreta():
+    def noticia_titulo_minusculo(self):
+        dicionario_titulo_minusculo = {
+            "titulo": "lorem ipsum",
+            "url": "http://aisudhiads",
+            "chamada": "asdasdad",
+            "data": "asdadads",
+            "url_imagem": "http://asduihasd.jpg"
+        }
+        return dicionario_titulo_minusculo
+
+    def test_noticia_titulo_minusculo(self):
+        try:
+            noticia = Noticia(self.noticia_titulo_minusculo())
+        except errors.Titulo_nao_capitalizado:
+            assert True
+
+    def noticia_url_sem_http(self):
+        dicionario_url_sem_http = {
+            "titulo": "Lorem ipsum",
+            "url": "aisudhiads",
+            "chamada": "asdasdad",
+            "data": "asdadads",
+            "url_imagem": "http://asduihasd.jpg"
+        }
+        return dicionario_url_sem_http
+
+    def test_noticia_url_sem_http(self):
+        try:
+            noticia = Noticia(self.noticia_url_sem_http())
+        except errors.Url_sem_http:
+            assert True
+    
+    def noticia_url_imagem_extensao_errada(self):
+        dicionario_url_imagem_extensao_errada = {
+            "titulo": "Lorem ipsum",
+            "url": "http://aisudhiads",
+            "chamada": "asdasdad",
+            "data": "asdadads",
+            "url_imagem": "http://asduihasd"
+        }
+        return dicionario_url_imagem_extensao_errada
+
+    def test_noticia_url_imagem_extensao_errada(self):
+        try:
+            noticia = Noticia(self.noticia_url_imagem_extensao_errada())
+        except errors.Imagem_url_sem_extensao:
+            assert True
